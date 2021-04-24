51282d30cbde928518a40f37deb4a88cfa03fbd1
ramvignesh-b <ramvigneshgreat@pec.edu>
2021-04-24 22:57:55 +0530

    dpymenu style

--- Cogs/help.py
+++ Cogs/help.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from dpymenus import Page, PaginatedMenu
 import json
 
 
@@ -10,13 +11,27 @@ class Help(commands.Cog):
 
     @commands.command()
     async def help(self, ctx:commands.Context):
-        embed = discord.Embed(title="List of commands", description="**__Prefix:__** `k!` ", color=discord.Colour.magenta())
+        page1 = Page(title="List of commands", description="__Page 1__: **General Commands**", color=discord.Colour.magenta())
         with open('assets/help.json', encoding='utf-8') as f:
             command_list = json.load(f)
-        split = len(command_list)
-        for i in range(0, split):
-            embed.add_field(name=f"{command_list[i]['command']}", value=command_list[i]['help'], inline=False)
-        await ctx.send(embed=embed)
+        for i in range(0, len(command_list)):
+            page1.add_field(name=f"{command_list[i]['command']}", value=command_list[i]['help'], inline=False)
+        
+        page2 = Page(title="List of commands", description="__Page 2__ **Image Commands**", color=discord.Colour.magenta())
+        with open('assets/images.json', encoding='utf-8') as f:
+            command_list = json.load(f)
+        for i in range(0, len(command_list)):
+            page2.add_field(name=f"{command_list[i]['command']}", value=command_list[i]['help'], inline=False)
+        
+        page3 = Page(title="List of commands", description="__Page 3__ **Animals Commands**", color=discord.Colour.magenta())
+        with open('assets/animals.json', encoding='utf-8') as f:
+            command_list = json.load(f)
+        for i in range(0, len(command_list)):
+            page3.add_field(name=f"{command_list[i]['command']}", value=command_list[i]['help'], inline=False)
+        
+        menu = PaginatedMenu(ctx).allow_multisession()
+        menu.add_pages([page1, page2, page3])
+        await menu.open()
 
 
 def setup(bot:commands.Bot):
