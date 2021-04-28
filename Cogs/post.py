c5fb9726e1218f5780776a391ce162d5933f1ab1
ramvignesh-b <ramvigneshgreat@pec.edu>
2021-04-28 11:48:22 +0530

    added sticker

--- Cogs/post.py
+++ Cogs/post.py
@@ -26,6 +26,16 @@ class Post(commands.Cog):
         await ctx.send(embed=discord.Embed(color=0x9c13ae).set_image(url=result_gif).set_footer(text=f"Requested by: {ctx.author.name}", icon_url=ctx.author.avatar_url))
 
 
+    @commands.command()
+    async def sticker(self, ctx:commands.Context, *, search: str):
+        search = search.replace(" ", "+")
+        response = requests.get('http://api.giphy.com/v1/stickers/search?q=' + search + '&api_key=' + GIPHY_TOKEN + '&limit=50' +'&rating=g')
+        data = json.loads(response.text)
+        gif_choice = random.randint(0, len(data['data'])-1)
+        result_gif = data['data'][gif_choice]['images']['original']['url']
+        await ctx.send(embed=discord.Embed(color=0x9c13ae).set_image(url=result_gif).set_footer(text=f"Requested by: {ctx.author.name}", icon_url=ctx.author.avatar_url))
+
+
     @commands.command(pass_context=True)
     async def woof(self, ctx):
         response = requests.get("https://dog.ceo/api/breeds/image/random")
